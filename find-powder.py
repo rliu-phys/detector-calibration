5590cca009fbbf0fd96f93bc7f3a3ff6ca13569b
Rui Liu <rliu.phys@gmail.com>
2024-11-11 16:13:50 -0600

    Rename file

--- find-powder.py
+++ find-powder.py
@@ -3,3 +3,6 @@ Nov 11, 2024 Developed by Rui Liu @ANL
 rui.liu@anl.gov
 This module is for detector callibration at 26ID at APS
 """
+import numpy as np
+import matplotlib.pyplot as plt
+from qbin_pilatus import qbin_pilatus
