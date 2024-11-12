c74ae1c352ab1ea4560a6a98c40241d2d55a10f8
Rui Liu <rliu.phys@gmail.com>
2024-11-12 14:01:46 -0600

    Debug

--- findpowder.py
+++ findpowder.py
@@ -18,7 +18,7 @@ def findpowder(x, img_exp, plotflag=True):
     if plotflag:
         # plot the experiment image
         plt.figure(700)
-        plt.imshow(img_exp, cmap='gray')
+        plt.imshow(img_exp, vmin = 0, vmax = 20, cmap='gray')
         plt.title("Raw Experimental Image")
         plt.colorbar()
         plt.axis('image')
@@ -50,7 +50,7 @@ def findpowder(x, img_exp, plotflag=True):
 # Example usage
 # Load your experimental image (replace with your actual image file path)
 # img_exp = np.random.rand(1024, 1062)  # Example placeholder image
-image_path = 'Images/475_0_0.tiff'
+image_path = 'Images/475_0_25.tiff'
 img_exp = imageio.imread(image_path)  # Replace with your image file path
 x = [1.8e5, -0.375e5, -0.30e5, 0.00037 * 1e5, 36.97]  # Example parameters
 
